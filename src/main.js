722dd74f42029d822088cdac5e90073dd4f0604f
a9043 <zzz13129180808@gmail.com>
2018-09-11 18:40:54 +0800

    CDN

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import request from './request'
-import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.prototype.$request = request
-
-Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    configureWebpack: {
+        externals: {
+            'element-ui': 'element-ui',
+            'vue': 'Vue',
+            'vuex': 'Vuex',
+            'vue-router': 'VueRouter'
+        }
+    }
+}
