fc36c7690681627c7cd62893e10df651219fb1ea
a9043 <zzz13129180808@gmail.com>
2018-09-15 19:22:48 +0800

    备案号

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,50 +1,61 @@
 <template>
-    <el-row class="login-box-row" type="flex" justify="center">
-        <el-col class="login-box-col" :xl="5" :xs="12" :span="6">
-            <h1 class="login-box-brand">Happy Sign In</h1>
-            <el-form class="login-form" ref="loginForm" :model="loginForm" :rules="loginRules">
-                <el-form-item label="用户名/学号/教工号" prop="suId">
-                    <el-input v-model="loginForm.suId" @keyup.enter.native="onSubmit"/>
-                </el-form-item>
-                <el-form-item label="密码" prop="suPassword">
-                    <el-input type="password" v-model="loginForm.suPassword" @keyup.enter.native="onSubmit"/>
-                </el-form-item>
-                <el-form-item>
-                    <el-button class="login-form-button" type="primary" @click="onSubmit" :loading="isLoginProcessing"
-                               :disabled="isLoginProcessing">
-                        登录
+    <el-container class="login-box-container">
+        <el-row class="login-box-row" type="flex" justify="center">
+            <el-col class="login-box-col" :xl="5" :xs="12" :span="6">
+                <h1 class="login-box-brand">Happy Sign In</h1>
+                <el-form class="login-form" ref="loginForm" :model="loginForm" :rules="loginRules">
+                    <el-form-item label="用户名/学号/教工号" prop="suId">
+                        <el-input v-model="loginForm.suId" @keyup.enter.native="onSubmit"/>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="suPassword">
+                        <el-input type="password" v-model="loginForm.suPassword" @keyup.enter.native="onSubmit"/>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button class="login-form-button" type="primary" @click="onSubmit"
+                                   :loading="isLoginProcessing"
+                                   :disabled="isLoginProcessing">
+                            登录
+                        </el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+            <el-dialog width="600px"
+                       title="多角色账户，请选择角色进入"
+                       :center="true"
+                       :visible="dialogVisible"
+                       :close-on-click-modal="false"
+                       :close-on-press-escape="false"
+                       :show-close="false">
+                <el-form class="login-dialog-form" label-position="left" label-width="100px">
+                    <el-form-item label="可选角色">
+                        <el-radio-group v-model="role">
+                            <el-radio v-show="authorities.includes('STUDENT')" :label="`STUDENT`">学生</el-radio>
+                            <el-radio v-show="authorities.includes('MONITOR')" :label="`MONITOR`">督导员</el-radio>
+                            <el-radio v-show="authorities.includes('TEACHER')" :label="`TEACHER`">老师</el-radio>
+                            <el-radio v-show="authorities.includes('ADMINISTRATOR')" :label="`ADMINISTRATOR`">管理员
+                            </el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    <el-form-item label="记住我选择">
+                        <el-radio-group v-model="remember">
+                            <el-radio :label="true">是</el-radio>
+                            <el-radio :label="false">否</el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    <el-button type="primary" @click="onSure">
+                        确定
                     </el-button>
-                </el-form-item>
-            </el-form>
-        </el-col>
-        <el-dialog width="600px"
-                   title="多角色账户，请选择角色进入"
-                   :center="true"
-                   :visible="dialogVisible"
-                   :close-on-click-modal="false"
-                   :close-on-press-escape="false"
-                   :show-close="false">
-            <el-form class="login-dialog-form" label-position="left" label-width="100px">
-                <el-form-item label="可选角色">
-                    <el-radio-group v-model="role">
-                        <el-radio v-show="authorities.includes('STUDENT')" :label="`STUDENT`">学生</el-radio>
-                        <el-radio v-show="authorities.includes('MONITOR')" :label="`MONITOR`">督导员</el-radio>
-                        <el-radio v-show="authorities.includes('TEACHER')" :label="`TEACHER`">老师</el-radio>
-                        <el-radio v-show="authorities.includes('ADMINISTRATOR')" :label="`ADMINISTRATOR`">管理员</el-radio>
-                    </el-radio-group>
-                </el-form-item>
-                <el-form-item label="记住我选择">
-                    <el-radio-group v-model="remember">
-                        <el-radio :label="true">是</el-radio>
-                        <el-radio :label="false">否</el-radio>
-                    </el-radio-group>
-                </el-form-item>
-                <el-button type="primary" @click="onSure">
-                    确定
-                </el-button>
-            </el-form>
-        </el-dialog>
-    </el-row>
+                </el-form>
+            </el-dialog>
+        </el-row>
+        <el-footer class="login-footer">
+            <el-row type="flex" justify="center">
+                <el-col :span="6">
+                    <el-button type="text" @click="toGov">粤ICP备18035033号-1</el-button>
+                </el-col>
+            </el-row>
+        </el-footer>
+    </el-container>
 </template>
 
 <script>
@@ -136,6 +147,9 @@
                     _this.$router.push('/index')
                     _this.isLoginProcessing = false
                 })
+            },
+            toGov() {
+                document.location = 'http://www.miitbeian.gov.cn'
             }
         }
     }
@@ -146,8 +160,12 @@
         content: none;
     }
 
-    .login-box-row {
+    .login-box-container {
         height: calc(100vh - 16px);
+    }
+
+    .login-box-row {
+        height: calc(100vh - 56px);
     }
 
     .login-box-col {
@@ -169,5 +187,10 @@
 
     .login-dialog-form {
         padding: 40px;
+    }
+
+    .login-footer {
+        text-align: center;
+        height: 40px;
     }
 </style>
